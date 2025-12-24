3f66bd40efaf5fd7116bf7311dd53963e697fe17
ayesha-shaikh-codes <ayeshashaikh8657@gmail.com>
2025-12-24 12:17:53 +0530

    Created q2.c

---
+++ programs/q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    printf("Enter 1st number");
+    scanf("%d",&a);
+    printf("Enter 2nd number");
+    scanf("%d",&b);
+    // Arithmatic Operatora
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a % b = %d\n", a % b);
+    printf("+a = %d\n", +a);
+    printf("-a = %d\n", -a);
+    printf("a++ = %d\n", a++);
+    printf("a-- = %d\n", a--);
+
+    return 0;
+}
+
