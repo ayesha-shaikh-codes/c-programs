e544d9b03be692263fdd220069730b03768b4714
ayesha-shaikh-codes <ayeshashaikh8657@gmail.com>
2025-12-24 12:21:53 +0530

    Created q3.c

---
+++ programs/q3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter 1st number");
+    scanf("%d",&a);
+    printf("Enter 2nd number");
+    scanf("%d",&b);
+    // Relational Operators
+    printf("a < b  : %d\n", a < b);
+    printf("a > b  : %d\n", a > b);
+    printf("a <= b: %d\n", a <= b);
+    printf("a >= b: %d\n", a >= b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+
+    return 0;
+}
