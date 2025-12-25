81556f85bec310fdd25fc4322b2cabed5fb9f7f6
ayesha-shaikh-codes <ayeshashaikh8657@gmail.com>
2025-12-25 17:06:35 +0530

    Updated q13.c

--- programs/q13.c
+++ programs/q13.c
@@ -1,4 +1,4 @@
-/* Program to reverse a number using do-while loop ( palindrome of a given number)*/
+/* Program to reverse a number using do-while loop */
 #include <stdio.h>
 int main() {
     int rev=0,digit,n,temp;
