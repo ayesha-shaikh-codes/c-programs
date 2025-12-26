250043023a9af565f7de1d472fd6e732b4b44d03
ayesha-shaikh-codes <ayeshashaikh8657@gmail.com>
2025-12-26 19:58:10 +0530

    Created q23.c

---
+++ programs/q23.c
@@ -0,0 +1,11 @@
+/* C program to declare,define and call a function */
+#include <stdio.h>
+void greetings(); // function declaration
+int main()
+{
+    greetings(); //function calling
+    return 0;
+}
+void greetings(){
+printf("Hello"); //function definition
+}
