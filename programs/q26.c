d4fedc1baf53c093f35c1f8e675067cc620023bc
ayesha-shaikh-codes <ayeshashaikh8657@gmail.com>
2025-12-26 20:19:56 +0530

    Created q26.c

---
+++ programs/q26.c
@@ -0,0 +1,23 @@
+/* C program to call by reference */
+#include <stdio.h>
+
+void swap(int *x, int *y)   // function definition
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    printf("Inside function: x = %d, y = %d\n", *x, *y);
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    swap(&a, &b);   // Call by Value
+    printf("Outside function: a = %d, b = %d\n", a, b);
+    return 0;
+}
+/*
+Inside function: x = 20, y = 10
+Outside function: a = 20, b = 10
+*/
