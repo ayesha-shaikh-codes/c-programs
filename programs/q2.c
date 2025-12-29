c0ea16190667a9fffd1a98deae84b626bfe74d58
ayesha-shaikh-codes <ayeshashaikh8657@gmail.com>
2025-12-29 16:40:58 +0530

    Updated q2.c

--- programs/q2.c
+++ programs/q2.c
@@ -3,11 +3,11 @@
 int main() {
 
     int a,b;
-    printf("Enter 1st number");
+    printf("Enter 1st number: ");
     scanf("%d",&a);
-    printf("Enter 2nd number");
+    printf("Enter 2nd number: ");
     scanf("%d",&b);
-    // Arithmatic Operatora
+    // Arithmatic Operators
     printf("a + b = %d\n", a + b);
     printf("a - b = %d\n", a - b);
     printf("a * b = %d\n", a * b);
@@ -20,4 +20,17 @@ int main() {
 
     return 0;
 }
+/*
+Enter 1st number: 25
+Enter 2nd number: 20
+a + b = 45
+a - b = 5
+a * b = 500
+a / b = 1
+a % b = 5
++a = 25
+-a = -25
+a++ = 25
+a-- = 26
+*/
 
